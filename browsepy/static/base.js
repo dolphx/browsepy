5932665728396dc957bea366e614b047112c2998
Jan Schmidt <schmidtja@nm.ifi.lmu.de>
2017-01-17 13:48:50 +0100

    Add terms and conditions

--- browsepy/static/base.js
+++ browsepy/static/base.js
@@ -21,42 +21,42 @@
 }());
 
 if ( !  readCookie("termsandconditions")) {
-        popupTerms();
+    popupTerms();
+}
+function createCookie(name,value,days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        var expires = "; expires=" + date.toUTCString();
     }
-        function createCookie(name,value,days) {
-            if (days) {
-                var date = new Date();
-                date.setTime(date.getTime() + (days*24*60*60*1000));
-                var expires = "; expires=" + date.toUTCString();
-            }
-            else var expires = "";
-            document.cookie = name + "=" + value + expires + "; path=/";
-        }
+    else var expires = "";
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
 
-        function readCookie(name) {
-            var nameEQ = name + "=";
-            var ca = document.cookie.split(';');
-            for(var i=0;i < ca.length;i++) {
-                var c = ca[i];
-                while (c.charAt(0)==' ') c = c.substring(1,c.length);
-                if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-            }
-            return null;
-        }
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
 
-        function eraseCookie(name) {
-            createCookie(name,"",-1);
-        }
-        function popupTerms()
+function eraseCookie(name) {
+    createCookie(name,"",-1);
+}
+function popupTerms()
+{
+    bootbox.confirm(terms, function(result){
+        if(result)
         {
-            bootbox.confirm("<strong>Terms and Conditions</strong> Please Accept the Terms and Conditions!", function(result){
-                if(result)
-                {
-                    createCookie("termsandconditions", "accepted", 365);
-                }
-                else
-                {
-                    popupTerms();
-                }
-            });
+            createCookie("termsandconditions", "accepted", 365);
         }
+        else
+        {
+            popupTerms();
+        }
+    });
+}
